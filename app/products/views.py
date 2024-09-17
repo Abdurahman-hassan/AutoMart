c900ca5008e80a7126f7cef581aa1bbf4da0d216
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-09-17 04:01:55 +0300

    add ProductViewSet and handle caching for List and retrieve for TTL 15 mins

---
+++ app/products/views.py
@@ -0,0 +1,62 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from app.products.models import Product
+from app.products.serializers import ProductSerializer
+from app.utils.permissions import IsAdminOrReadOnly
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+
+from config.settings.base import CACHE_TTL
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminOrReadOnly]
+
+    @extend_schema(
+        operation_id="List Products",
+        description="Retrieve a list of all products",
+        responses={200: ProductSerializer(many=True)},
+    )
+    @method_decorator(cache_page(CACHE_TTL))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        operation_id="Retrieve Product",
+        description="Retrieve details of a specific product by SKU",
+        responses={200: ProductSerializer},
+    )
+    @method_decorator(cache_page(CACHE_TTL))
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        operation_id="Create Product",
+        description="Create a new product",
+        responses={200: ProductSerializer},
+    )
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        cache.clear()
+
+    @extend_schema(
+        operation_id="Update Product",
+        description="Update an existing product",
+        responses={200: ProductSerializer},
+    )
+    def perform_update(self, serializer):
+        super().perform_update(serializer)
+        cache.clear()
+
+    @extend_schema(
+        operation_id="Delete Product",
+        description="Delete an existing product",
+        responses={200: ProductSerializer},
+    )
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+        cache.clear()
